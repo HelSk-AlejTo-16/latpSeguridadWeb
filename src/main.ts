67a8b44fe45daf62d9a9e28c27b2c67ca874d4f5
AplicacionAndroidCRUD <leyotorres1501@gmail.com>
2026-03-02 10:00:00 -0600

    fix:conexión a base de datos y CRUD completo. Uso de swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   whitelist:true;
+
+  //Configuración de swagger.
+const config = new DocumentBuilder().setTitle('API cin vulnerabilidades de Seguridad')
+.setDescription('Documentación de la API para pruebas')
+.setVersion('1.0.0')
+.addTag('task').
+build();
+
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api/docs',app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/tasks/controller/task.controller.ts
+++ src/tasks/controller/task.controller.ts
@@ -1,10 +1,18 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { TaskService } from '../service/task.service'
+import { updateTaskDTO } from 'src/auth/dto/updateClassDTO';
+import { Task } from 'src/auth/entity/task.entity';
+import { STATUS_CODES } from 'http';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags("Tasks")
+
 
 @Controller('api/task')
 export class TaskController {
   constructor(private readonly taskSvc: TaskService) { }
   @Get()
+  @ApiOperation({summary: '|Lista las tareas disponibles'})
   public async fetchTasks(): Promise<any[]> {
     return await this.taskSvc.getTasks();
   }
@@ -15,9 +23,9 @@ export class TaskController {
   // ' UNION SELECT * FROM users
 
   @Get(":id")
-  public async getTaskById(@Param("id", ParseIntPipe) id: number):Promise <any> {
+  public async getTaskById(@Param("id", ParseIntPipe) id: number):Promise <Task> {
     var task =  await this.taskSvc.getTaskById(id);
-    if (task && task.length > 0) return task;
+    if (task) return task;
 
     else throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
     
@@ -29,12 +37,18 @@ export class TaskController {
     return this.taskSvc.insertTask(task);
   }
   @Put(":id")
-  public updateTask(@Body("id", ParseIntPipe) id: number, task: any): any {
+  public updateTask(@Body("id", ParseIntPipe) id: number, task: updateTaskDTO): any {
     return this.taskSvc.updateTask(id, task);
   }
   @Delete()
-  public deleteTask(@Param("id", ParseIntPipe) id: number): any {
-    return this.taskSvc.deleteTask(id);
+  @HttpCode(HttpStatus.OK)
+  public async deleteTask(@Param("id", ParseIntPipe) id: number):Promise <boolean> {
+    
+    const result = await this.taskSvc.deleteTask(id);
+
+    if(!result)
+      throw new HttpException("No se puede eliminar la tarea", HttpStatus.INTERNAL_SERVER_ERROR)
+    return result;
   }
 
 
